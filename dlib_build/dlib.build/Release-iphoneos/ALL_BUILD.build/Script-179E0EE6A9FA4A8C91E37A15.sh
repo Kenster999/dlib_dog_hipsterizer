f576b822f46a43d038daf6715fc08f17d6106026
Kenster999 <32493770+Kenster999@users.noreply.github.com>
2018-10-02 13:43:19 -0700

    Final set of original files

---
+++ dlib_build/dlib.build/Release-iphoneos/ALL_BUILD.build/Script-179E0EE6A9FA4A8C91E37A15.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/ken/projects/dlib/v-19.15-Xcode/dlib-19.15/examples/build/dlib_build -f /Users/ken/projects/dlib/v-19.15-Xcode/dlib-19.15/examples/build/dlib_build/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
